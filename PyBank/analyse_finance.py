7c4cf9d7c1cde2becd965e31ca89b451c098fc1e
Tim <Tim.A.Ung.12@gmail.com>
2022-12-05 18:31:13 +1100

    Calculated the greatest profit increase and the smallest profit decrease

--- PyBank/analyse_finance.py
+++ PyBank/analyse_finance.py
@@ -1,9 +1,20 @@
 import csv
 
+'''
+TODO:
+DONE - The total number of months included in the dataset
+DONE - The net total amount of "Profit/Losses" over the entire period
+DONE - The changes in "Profit/Losses" over the entire period, and then the average of those changes
+DONE - The greatest increase in profits (date and amount) over the entire period
+DONE - The greatest decrease in profits (date and amount) over the entire period
+'''
+
 def analyse_finance(csvpath):
     num_months = 0
     net_total = 0
     avg_change = 0
+    max_profit_increase = [None, float('-inf')]
+    min_profit_decrease = [None, float('inf')]
 
     with open(csvpath, encoding='utf8') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
@@ -22,6 +33,12 @@ def analyse_finance(csvpath):
                 change = float(row[1]) - prev_row
                 avg_change += change
                 prev_row = float(row[1])
+
+                # Finds the Greatest profit increase & Decrease
+                if change > float(max_profit_increase[1]):
+                    max_profit_increase = [row[0], round(change)]
+                elif change < float(min_profit_decrease[1]):
+                    min_profit_decrease = [row[0], round(change)]
 
         avg_change = round(avg_change / (num_months - 1), 2)
 
