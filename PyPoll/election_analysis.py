79294fb0d0b29ba72648d20298e2f76669729f02
Tim <Tim.A.Ung.12@gmail.com>
2022-12-07 11:13:37 +1100

    Exported output to csv | Print output in terminal | Task Completed

--- PyPoll/election_analysis.py
+++ PyPoll/election_analysis.py
@@ -1,13 +1,6 @@
 import csv
 
-'''
-CSV breakdown:
-- Voter ID
-- County
-- Candidate
-'''
-
-def election_analysis(csvpath:str):
+def election_analysis(csvpath:str, outputpath:str):
     poll = {}
 
     # Read through the csv file and add the candidate and their number of votes into a dictionary
@@ -21,15 +14,31 @@ def election_analysis(csvpath:str):
             else:
                 poll[row[2]] = 1
 
+    # Sum the number of votes each candidate got | found the highest number of votes
     total_votes = sum(poll.values())
     winner = max(poll, key=poll.get)
 
-    print(poll)
-    print(winner)
-      
-        
-
-
+    # Export results to csv file
+    with open(outputpath, 'w', encoding='utf8', newline='') as outputfile:
+        outputwriter = csv.writer(outputfile, delimiter=' ')
 
         
-        
+        outputwriter.writerow("Election Results".split(' '))
+        outputwriter.writerow(["-------------------------"])
+
+        outputwriter.writerow(f"Total Votes: {total_votes}".split(' '))
+        outputwriter.writerow(["-------------------------"])
+
+        for candidate in poll:
+            outputwriter.writerow(f"{candidate}: {round((poll[candidate]/total_votes) * 100, 3)}% ({poll[candidate]})".split(' '))
+        outputwriter.writerow(["-------------------------"])
+
+        outputwriter.writerow(f"Winner: {winner}".split(' '))
+        outputwriter.writerow(["-------------------------"])
+    
+    # Print the output
+    with open(outputpath, 'r', encoding='utf8') as printfile:
+        printreader = csv.reader(printfile, delimiter='\n')
+
+        for row in printreader:
+            print(row[0])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,5 +3,6 @@ from election_analysis import election_analysis as ea
 
 if __name__ == "__main__":
     csvpath = os.path.join('.', 'Resources', 'election_data.csv')
+    outputpath = os.path.join('.', 'analysis', 'election_analysis.csv')
     
-    ea(csvpath)
+    ea(csvpath, outputpath)
